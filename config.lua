c3f0bd269d2c42fade2d3207886921008831ff0d
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-03-15 17:51:58 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,90 @@
+Config = {}
+
+Config.Stables = {
+	Valentine = {
+		Pos = vector3(-367.73, 787.72, 116.26),
+		Name = 'Stable Valentine',
+        	Heading = -30.65,
+		SpawnPoint = {
+			Pos = vector3(-372.43, 791.79, 116.13),
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 182.3
+        }
+    },
+	Blackwater = {
+		Pos = vector3(-864.84, -1365.96, 43.54),
+		Name = 'Stable Blackwater',
+        	Heading = -30.65,
+		SpawnPoint = {
+			Pos = vector3(-867.74, -1361.69, 43.66),
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 178.59
+        }
+    },
+	SaintDenis = {
+		Pos = vector3(2503.13, -1449.08, 46.3),
+		Name = 'Stable Saint Denis',
+        	Heading = -30.65,
+		SpawnPoint = {
+			Pos = vector3(2508.41, -1446.89, 46.4),
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 87.88
+        }		
+	},
+	Annesburg = {
+		Pos = vector3(2972.35, 1425.35, 44.67),
+		Name = 'Stable Annesburg',
+        	Heading = -30.65,
+		SpawnPoint = {
+			Pos = vector3(2970.43, 1429.35, 44.7),
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 223.94
+        }		
+	},
+	Rhodes = {
+		Pos = vector3(1321.46, -1358.66, 78.39),
+		Name = 'Stable Rhodes',
+        	Heading = -30.65,
+		SpawnPoint = {
+			Pos = vector3(1318.74, -1354.64, 78.18),
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 249.45
+        }		
+	},
+	Tumbleweed = {
+		Pos = vector3(-5519.43, -3043.45, -2.39),
+		Name = 'Stable Tumbleweed',
+        	Heading = 0.0,
+		SpawnPoint = {
+			Pos = vector3(-5522.14, -3039.16, -2.29),
+			CamPos = {x=1, y=-3, z=0},
+			Heading = 189.93
+        }		
+	},		
+}
+
+Config.Horses = {
+	{
+		name = "Category 1",
+		["A_C_HORSEMULE_01"] = {"Mula", 18, 18},
+		["A_C_Horse_KentuckySaddle_Grey"] = {"Kentucky Saddle (Cinza)", 50, 50},
+		["A_C_Horse_Morgan_Palomino"] = {"Morgan", 60, 60},
+		["A_C_Horse_Thoroughbred_DappleGrey"] = {"Thoroughbred", 130, 130},
+		["A_C_Horse_AmericanStandardbred_Buckskin"] = {"Standardbred Americano", 130, 130},
+		["A_C_Horse_AmericanPaint_Overo"] = {"American Paint (Cor-de-pinto)", 130, 130},
+		["A_C_Horse_Andalusian_DarkBay"] = {"Andalusian (Preto Cintilante)", 140, 140},
+		["A_C_Horse_Andalusian_Perlino"] = {"Andalusian (Celeste)", 140, 140},		
+		["A_C_Horse_Nokota_ReverseDappleRoan"] = {"Nokota, o ágil.", 450, 450},
+
+	},
+	{
+		name = "Category 2",
+		["A_C_Horse_Belgian_MealyChestnut"] = {"Belga (Loiro)", 120, 120},
+		["A_C_Horse_Belgian_BlondChestnut"] = {"Belga (Castanho-Claro)", 120, 120},
+		["A_C_Horse_Shire_LightGrey"] = {"Shire (Branco Acizentado)", 130, 130},
+		["A_C_Horse_AmericanPaint_Tobiano"] = {"American Paint (Tobiano)", 140, 140},
+		["A_C_Horse_SuffolkPunch_RedChestnut"] = {"Suffolk Punch (Castanho Avermelhado)", 150, 150},
+		["A_C_Horse_Appaloosa_Blanket"] = {"Appaloosa", 200, 200},
+		["A_C_Horse_Turkoman_Gold"] = {"Turkoman (Dourado Fulgente)", 950, 950},
+	}
+}
