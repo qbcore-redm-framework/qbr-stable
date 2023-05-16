7aeb917ae254169aeea3f287ac3d1d1acb537eda
gononono64 <dlad9_2012@hotmail.com>
2023-05-16 05:39:42 -0500

    :hammer: Quick fix
    Made stables functional
    after selecting your horse and confirming press h (whistle) to summon the selected horse
    f to dismiss
    Also made Max Number Of Horses  a config option instead of flat 3

--- server/main.lua
+++ server/main.lua
@@ -67,8 +67,8 @@ RegisterNetEvent("qbr-stable:BuyHorse", function(data, name)
 	local Playercid = Player.PlayerData.citizenid
 
 	MySQL.query('SELECT * FROM horses WHERE `cid`=@cid;', {cid = Playercid}, function(horses)
-		if #horses >= 3 then
-			TriggerClientEvent('QBCore:Notify', src, 9, "You can have a maximum of 3 horses!", 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
+		if #horses >= Config.MaxNumberOfHorses then
+			TriggerClientEvent('QBCore:Notify', src, 9, "You can have a maximum of "..Config.MaxNumberOfHorses.." horses!", 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
 			return
 		end
 		Wait(200)
@@ -78,14 +78,14 @@ RegisterNetEvent("qbr-stable:BuyHorse", function(data, name)
 				local bank = Player.Functions.RemoveMoney("bank", data.Gold, "stable-bought-horse")
 				TriggerEvent('qbr-log:server:CreateLog', 'shops', 'Stable', 'green', "**"..GetPlayerName(Player.PlayerData.source) .. " (citizenid: "..Player.PlayerData.citizenid.." | id: "..Player.PlayerData.source..")** bought a horse for $"..data.Gold..".")
 			else
-				print('not enough money')
+				TriggerClientEvent('QBCore:Notify', src, 9, "Not enough money!", 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
 				return
 			end
 		else
 			if Player.Functions.RemoveMoney("cash", data.Dollar, "stable-bought-horse") then
 				TriggerEvent('qbr-log:server:CreateLog', 'shops', 'Stable', 'green', "**"..GetPlayerName(Player.PlayerData.source) .. " (citizenid: "..Player.PlayerData.citizenid.." | id: "..Player.PlayerData.source..")** bought a horse for $"..data.Dollar..".")
 			else
-				print('not enough money')
+				TriggerClientEvent('QBCore:Notify', src, 9, "Not enough money!", 5000, 0, 'mp_lobby_textures', 'cross', 'COLOR_WHITE')
 				return
 			end
 		end
@@ -148,4 +148,25 @@ RegisterNetEvent("qbr-stable:SellHorseWithId", function(id)
 			end
 		end
 	end)
+end)
+
+
+function CreateDatabaseIfNotExists()
+	MySQL.query.await([[
+		CREATE TABLE IF NOT EXISTS `horses` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		`cid` varchar(50) NOT NULL,
+		`selected` int(11) NOT NULL DEFAULT 0,
+		`model` varchar(50) NOT NULL,
+		`name` varchar(50) NOT NULL,
+		`components`  varchar(5000) NOT NULL DEFAULT '{}',
+		PRIMARY KEY (`id`))
+		]]
+	)
+end
+
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		CreateDatabaseIfNotExists()
+	end
 end)
